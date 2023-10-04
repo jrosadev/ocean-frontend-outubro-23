b565c8fded8ce92a3191031a2c9e0618b6fe3dfb
Júlia Ramos <jrosadev@gmail.com>
2023-10-04 14:49:47 +0000

    Estilização do card

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="card">
         <h2>Sailor Moon</h2>
-        <img src="https://akamai.sscdn.co/uploadfile/letras/fotos/3/7/3/1/37311c79b3c8316ce05f3b69f8a8ca4f.jpg"/>
-      </>
+        <img src="https://akamai.sscdn.co/uploadfile/letras/fotos/3/7/3/1/37311c79b3c8316ce05f3b69f8a8ca4f.jpg" />
+      </div>
     </>
   )
 }
