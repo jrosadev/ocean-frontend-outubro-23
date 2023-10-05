488e8d6c26a8ae879a3163b7e82ede30dfebfe87
Júlia Ramos <jrosadev@gmail.com>
2023-10-05 15:30:57 +0000

    Desafio HTML + CSS e desafio array + map

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
-import './App.css'
+import './App.css';
 import Card from './components/Card/Card'
 
 function App() {
   const personagem1 = {
     nome: 'Sailor Moon',
-    imagemUrl: 'https://static.wikia.nocookie.net/sailormoon/images/1/1c/Usagi_Sailor_Moon_Cosmic_Form_-_Anime.png/revision/latest/scale-to-width-down/305?cb=20190909081231'
+    imagemUrl: 'https://static.wikia.nocookie.net/sailormoon/images/1/1c/Usagi_Sailor_Moon_Cosmic_Form_-_Anime.png/revision/latest/scale-to-width-down/305?cb=20190909081231',
+    tags: ['Nome: Usagi Tsukino', 'Aniversário: 30/06', 'Ocupação: estudante']
   }
 
   const personagem2 = {
     nome: 'Sailor V',
-    imagemUrl: 'https://static.wikia.nocookie.net/sailormoon/images/4/41/Minako_Aino_Sailor_Form_-_Anime.png/revision/latest/scale-to-width-down/309?cb=20190906073411'
+    imagemUrl: 'https://static.wikia.nocookie.net/sailormoon/images/4/41/Minako_Aino_Sailor_Form_-_Anime.png/revision/latest/scale-to-width-down/309?cb=20190906073411',
+    tags: ['Nome: Minako Aino', 'Aniversário: 22/10', 'Ocupação: estudante']
   }
 
-  const personagens = [personagem1, personagem2]
+  const personagem3 = {
+    nome: 'Sailor Marte',
+    imagemUrl: 'https://static.wikia.nocookie.net/sailormoon/images/b/be/Rei_Hino_Sailor_Mars_Sailor_Form_-_Anime.png/revision/latest/scale-to-width-down/254?cb=201909042324260',
+    tags: ['Nome: Rei Hino', 'Aniversário: 17/04', 'Ocupação: estudante']
+  }
+
+  const personagens = [personagem1, personagem2, personagem3]
 
   return (
     <>
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,21 @@
+import Tag from '../Tag/Tag';
 import './Card.css'
 
 export default function Card(props) {
     const item = props.item;
 
+    const tags = item.tags || [];
+
     return(
         <div className="card">
             <h2>{item.nome}</h2>
+
+            <div className="tags-list">
+                {tags.map(function (tag) {
+                    return <Tag text={tag} />
+                })}          
+            </div>
+
         <img src={item.imagemUrl} />
       </div>
     )
