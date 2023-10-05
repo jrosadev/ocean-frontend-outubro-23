e961bc8aabbbd34493d47627b26d54ad363d6d91
Júlia Ramos <jrosadev@gmail.com>
2023-10-05 13:55:35 +0000

    Adição das props no card

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import './App.css'
+import Card from './components/Card/Card'
 
 function App() {
+  const personagem1 = {
+    nome: 'Sailor Moon',
+    imagemUrl: 'https://static.wikia.nocookie.net/sailormoon/images/1/1c/Usagi_Sailor_Moon_Cosmic_Form_-_Anime.png/revision/latest/scale-to-width-down/305?cb=20190909081231'
+  }
+
+  const personagem2 = {
+    nome: 'Sailor V',
+    imagemUrl: 'https://static.wikia.nocookie.net/sailormoon/images/4/41/Minako_Aino_Sailor_Form_-_Anime.png/revision/latest/scale-to-width-down/309?cb=20190906073411'
+  }
 
   return (
     <>
-      <div className="card">
-        <h2>Sailor Moon</h2>
-        <img src="https://akamai.sscdn.co/uploadfile/letras/fotos/3/7/3/1/37311c79b3c8316ce05f3b69f8a8ca4f.jpg" />
-      </div>
+      <Card item={personagem1} />
+      <Card item={personagem2} />
     </>
+
   )
 }
 
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,12 @@
+import './Card.css'
+
+export default function Card(props) {
+    const item = props.item;
+
+    return(
+        <div className="card">
+            <h2>{item.nome}</h2>
+        <img src={item.imagemUrl} />
+      </div>
+    )
+}
